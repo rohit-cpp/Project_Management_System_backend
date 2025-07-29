2c6ed4216876e86fe3cf8e5f40a84b962b663606
Rohit <rrgawanderohit@gmail.com>
2025-07-29 15:38:29 +0530

    issue state done

--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -4,11 +4,13 @@ import { authReducer } from "./Auth/Reducer";
 import { projectReducer } from "./Project/Reducer";
 import ChatReducer from "./Chat/Reducer";
 import commentReducer from "./Comment/Reducer";
+import issueReducer from "./Issue/Reducer";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   project: projectReducer,
   chat: ChatReducer,
   comment: commentReducer,
+  issue: issueReducer,
 });
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
